8a474c5e10f8b1890ecce480be3ff514fc396256
Bowen <huangbowen521@gmail.com>
2012-11-12 08:24:45 +0800

    add own controller and test code.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,10 +3,30 @@
 /* Controllers */
 
 
-function MyCtrl1() {}
-MyCtrl1.$inject = [];
+	angular.module('myApp.controllers', []).controller('MyCtrl1', function($scope){
+	
+	$scope.msg= "this is the message";
+	
+	$scope.showAlert = function(msg){
+		alert(msg);
+	};
+}).controller('UpdateHomeOfficeController',
+function ($scope, $routeParams, getConsultantInfo, allOffices){
+	getConsultantInfo($routeParams.consultantId, function (consultant){
+		$scope.fullname = consultant.last + ' ' + consultant.first;
+	});
 
+	$scope.countryNames = [];
+	angular.forEach(allOffices, function(office){
+		$scope.countryNames.push(office.country);
+	});
+	$scope.countryNames.sort();
 
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
+	$scope.$watch('selectedCountry', function(value){
+		angular.forEach(allOffices, function(country){
+			if (country.country === value){
+				$scope.officeNames = country.offices;
+				$scope.officeNames.sort();			}
+		});
+	});
+});
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,30 +2,42 @@
 
 /* jasmine specs for controllers go here */
 
-describe('MyCtrl1', function(){
-  var myCtrl1;
+describe('UpdateHomeOfficeController', function(){
+  var scope,
+  consultantId= '9999',
+  routeParams = { consultantId: consultantId},
+  getConsultantInfo,
+  allOffices= [
+    {country:'China', offices:['Beiijng', 'Chengdu', 'Xian', 'Shanghai']},
+    {country:'Canada', offices:['bala', 'dfdf']}
+  ];
 
-  beforeEach(function(){
-    myCtrl1 = new MyCtrl1();
+  beforeEach(module('myApp.controllers'));
+
+  beforeEach(inject(function($rootScope, $controller){
+     getConsultantInfo = function(id, callback){
+      if(id ==='9999') callback({first:'bowen',last:'you'});
+     };
+
+     scope = $rootScope.$new();
+     var controller = $controller('UpdateHomeOfficeController',{
+      $scope:scope,
+      $routeParams:routeParams,
+      getConsultantInfo:getConsultantInfo,
+      allOffices:allOffices
+     });
+
+  }));
+
+
+  it ('should show the list of countries where we have offices', function(){
+    expect(scope.countryNames).toEqual(['Canada', 'China']);
   });
 
+  if('shoulde show the list of offices when we select a country', function(){
+    scope.selectedCountry = 'China',
 
-  it('should ....', function() {
-    //spec body
-  });
-});
-
-
-describe('MyCtrl2', function(){
-  var myCtrl2;
-
-
-  beforeEach(function(){
-    myCtrl2 = new MyCtrl2();
+    expect(scope.officeNames).toEqual(['Beijing','Chengdu','Xian','Shanghai']);
   });
 
-
-  it('should ....', function() {
-    //spec body
-  });
 });
